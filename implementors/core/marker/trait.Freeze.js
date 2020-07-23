84584229ee45d893e7bae2dce06eee5b4c6ae9c9
Travis CI User <travis@example.org>
2020-07-23 14:39:35 +0000

    Documentation for indiv0/lazycell@f810c1e

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["lazycell"] = [{"text":"impl&lt;T&gt; !Freeze for <a class=\"struct\" href=\"lazycell/struct.LazyCell.html\" title=\"struct lazycell::LazyCell\">LazyCell</a>&lt;T&gt;","synthetic":true,"types":["lazycell::LazyCell"]},{"text":"impl&lt;T&gt; !Freeze for <a class=\"struct\" href=\"lazycell/struct.AtomicLazyCell.html\" title=\"struct lazycell::AtomicLazyCell\">AtomicLazyCell</a>&lt;T&gt;","synthetic":true,"types":["lazycell::AtomicLazyCell"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
