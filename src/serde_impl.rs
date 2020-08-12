d960ed4a2b566b185408ade228cbc50e672909a7
Nikita Pekin <nikita@ternary.io>
2020-08-11 20:49:27 -0400

    chore: Update license headers

--- src/serde_impl.rs
+++ src/serde_impl.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2020 Nikita Pekin and the lazycell contributors
+// See the README.md file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.use serde::ser::{Serialize, Serializer};
 use serde::ser::{Serialize, Serializer};
 use serde::de::{self, Deserialize, Deserializer, Visitor};
 
